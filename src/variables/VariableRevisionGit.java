9de987d703c851c208eee5928058d8f6bda4aea2
Kapoor27 <122999145+Kapoor27@users.noreply.github.com>
2024-02-02 20:48:25 -0300

    Variable Printing Var Int Double 1

---
+++ src/variables/VariableRevisionGit.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class VariableRevisionGit {
+
+    public static void main(String[] args){
+
+        //SYNTAX : Datatype varName;
+        String name;
+
+        //varName=value;
+        name="Rajeev";
+        System.out.println("Printing variable name :"+name);
+
+        int age;
+        age=36;
+        System.out.println("Printing age :"+age);
+
+        double salary;
+        salary= 90000.56;
+        System.out.println("Printing salary :"+salary);
+
+    }
+
+
+}
