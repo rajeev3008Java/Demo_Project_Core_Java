f58073df6313526f286938ace9c3ed92f2509b03
Kapoor27 <122999145+Kapoor27@users.noreply.github.com>
2024-02-02 19:40:04 -0300

    Initial commit

---
+++ src/revision/DemoPractice.java
@@ -0,0 +1,12 @@
+package revision;
+
+public class DemoPractice {
+
+    public static void main(String[] args){
+
+        System.out.println("My name is rajeev kapoor");
+
+        System.out.println("THIS IS PROGRAMING LANGUAGE");
+    }
+
+}
---
+++ src/variables/VariableRevisions.java
@@ -0,0 +1,29 @@
+package variables;
+
+public class VariableRevisions {
+
+    public static void main(String[]args){
+
+        //SYNTAX ; Datatype varName;
+        String name;
+
+        //varName=value;
+        name="JAVA";
+        System.out.println("Printing variable name ;"+name);
+
+        int age;
+        age=38;
+        System.out.println("Printing age :"+age);
+
+        double salary;
+        salary=98765.56;
+        System.out.println("Printing salary :"+salary);
+
+        String cityName;
+        cityName="Pune";
+        System.out.println("Printing cityName :"+cityName);
+
+    }
+
+
+}
