066e726fa206732f125a1956d64d17796cacbc68
Kapoor27 <122999145+Kapoor27@users.noreply.github.com>
2024-02-02 21:00:45 -0300

    Variable Printing Var Int Double 2

---
+++ src/variables/Variable_Revision_Checkout.java
@@ -0,0 +1,26 @@
+package variables;
+
+public class Variable_Revision_Checkout {
+
+    public static void main(String[] args){
+
+        //SYNTAX : Datatype varName
+        String name;
+
+        //varName=value;
+        name="PUNE";
+        System.out.println("Printing variable name :"+name);
+
+        int age;
+        age=40;
+        System.out.println("Printing age :"+age);
+
+        double salary;
+        salary=75435.89;
+        System.out.println("Printing salary :"+salary);
+
+
+    }
+
+
+}
